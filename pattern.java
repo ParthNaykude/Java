0d8a4e6cc3b140a0152bb747d378199f463bdb96
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-17 19:20:23 +0530

    Star pattern

---
+++ pattern.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class pattern{
+    public static void main(String[] args){
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
