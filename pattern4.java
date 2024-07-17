caec47c27719038c2b014442b8f7c8c833b0cbae
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-17 19:22:17 +0530

    Star pattern

---
+++ pattern4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class pattern4{
+    public static void main(String [] args){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
