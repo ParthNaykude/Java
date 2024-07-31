e61c0e7567a6d3c8affe6a2ae3298a5a2b5ced78
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-31 22:09:23 +0530

    Problem 3

---
+++ Q_three.java
@@ -0,0 +1,31 @@
+//Q3
+import java.util.*;
+
+class Q_three{
+
+    public static void main(String [] args ){
+
+        Scanner sc = new Scanner(System.in);
+        double radius;
+
+        double circumference ;
+
+
+        System.out.println("Enter the radius of circle : ");
+        radius = sc.nextInt();
+
+        circumference = 2*Math.PI*radius;
+
+        System.out.print("The circumference of given circle is : " + circumference);
+
+
+
+
+
+
+
+    }
+
+
+
+}
